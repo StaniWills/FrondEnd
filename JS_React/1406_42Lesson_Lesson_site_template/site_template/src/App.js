658c5ad89a6035826cc2cc9ce088061b6f1a6edc
StaniWills <deberkader@mail.ru>
2023-02-06 13:02:59 +0100

    Lesson 42 site template

---
+++ JS_React/1406_42Lesson_Lesson_site_template/site_template/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Banner1 from './components/Banner1';
+import Header from './components/Header';
+import Products from './components/Products';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Banner1 />
+      <Products />
+    </div>
+  );
+}
+
+export default App;

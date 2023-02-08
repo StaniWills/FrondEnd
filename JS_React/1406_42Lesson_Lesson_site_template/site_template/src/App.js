32f52f5b03dbf05c106637bbf7b694bf55fcc1a7
StaniWills <deberkader@mail.ru>
2023-02-08 13:35:35 +0100

    plans block added

--- JS_React/1406_42Lesson_Lesson_site_template/site_template/src/App.js
+++ JS_React/1406_42Lesson_Lesson_site_template/site_template/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Banner1 from './components/Banner1';
+import Banner2 from './components/Banner2';
 import Header from './components/Header';
+import Plans from './components/Plans';
 import Products from './components/Products';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
       <Header />
       <Banner1 />
       <Products />
+      <Banner2 />
+      <Plans />
     </div>
   );
 }

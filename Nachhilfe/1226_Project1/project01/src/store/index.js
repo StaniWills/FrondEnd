b9fbf53c99b2085b9307b65aeb5b9582afc08522
Stani Wills <107476765+StaniWills@users.noreply.github.com>
2023-03-03 13:28:35 +0100

    Delete Nachhilfe directory

--- Nachhilfe/1226_Project1/project01/src/store/index.js
+++
@@ -1,7 +0,0 @@
-import { createStore, combineReducers } from 'redux'
-
-const rootReducers =  combineReducers({
-    users: userReducer
-}) //набор функций для наполнения store
-
-export const store = createStore(rootReducers) //cоздаем ячейку для хранения иформации
